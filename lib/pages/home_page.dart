0555065af6dbd5be4d11bf1eda3c812e0d5e63c7
Henrique França <euhenriquefranca@gmail.com>
2022-02-09 20:40:03 -0300

    feat: Adiciona SharedPreferences

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project_flutter/controllers/home_controller.dart';
 import 'package:project_flutter/models/post_model.dart';
 import 'package:project_flutter/repositories/home_repository_imp.dart';
+import 'package:project_flutter/services/prefs_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,6 +25,16 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              PrefsService.logout();
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/login', (_) => true);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
